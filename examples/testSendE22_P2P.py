c3df0fc5b15832c685d4c52c035324a70d4825d5
matthias-bs <83612361+matthias-bs@users.noreply.github.com>
2021-10-13 09:23:02 +0200

    Add files via upload

--- examples/testSendE22_P2P.py
+++ examples/testSendE22_P2P.py
@@ -5,6 +5,7 @@
 # sending fixed point to point
 ###########################################
 # transmitter - address 0001 - channel 02
+# message     - address 0003 - channel 04
 # receiver    - address 0003 - channel 04
 ###########################################
 
@@ -15,9 +16,12 @@ M0pin = 25
 M1pin = 26
 AUXpin = 14
 
-e22 = ebyteE22(M0pin, M1pin, AUXpin, Address=0x0001, Channel=0x02, debug=False)
+# Address=0x0001 / Channel=0x02
+e22 = ebyteE22(M0pin, M1pin, AUXpin, Port='U2', Address=0x0001, Channel=0x02, debug=False)
 
 e22.start()
+
+e22.getConfig()
 
 to_address = 0x0003
 to_channel = 0x04
